354b33f16ed98453d4828a51a33d2a7177afae8c
tommy <tommy.lin@yxunistar.com>
2022-09-27 13:26:24 +0800

    spring-getting-started-guides

--- 05-gs-batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ 05-gs-batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,5 +1,7 @@
 package com.example.batchprocessing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -7,13 +9,15 @@ import org.springframework.context.support.AbstractApplicationContext;
 @SpringBootApplication
 public class BatchProcessingApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BatchProcessingApplication.class);
+
 	public static void main(String[] args) throws Exception {
 		AbstractApplicationContext applicationContext = (AbstractApplicationContext) SpringApplication.run(BatchProcessingApplication.class, args);
 		applicationContext.getBeanFactory()
 			.getBeanNamesIterator()
 			.forEachRemaining(beanName -> {
 				Object bean = applicationContext.getBean(beanName);
-				System.out.println(beanName + " : " + bean.getClass().getName());
+				log.warn(beanName + " : " + bean.getClass().getName());
 			});
 
 		System.exit(SpringApplication.exit(applicationContext));
