4a1caf967dc593134066df3e93cf3dd217611d43
tommy <tommy.lin@yxunistar.com>
2022-09-27 13:24:14 +0800

    spring-getting-started-guides

--- 05-gs-batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ 05-gs-batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -2,11 +2,20 @@ package com.example.batchprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.AbstractApplicationContext;
 
 @SpringBootApplication
 public class BatchProcessingApplication {
 
 	public static void main(String[] args) throws Exception {
-		System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
+		AbstractApplicationContext applicationContext = (AbstractApplicationContext) SpringApplication.run(BatchProcessingApplication.class, args);
+		applicationContext.getBeanFactory()
+			.getBeanNamesIterator()
+			.forEachRemaining(beanName -> {
+				Object bean = applicationContext.getBean(beanName);
+				System.out.println(beanName + " : " + bean.getClass().getName());
+			});
+
+		System.exit(SpringApplication.exit(applicationContext));
 	}
 }
