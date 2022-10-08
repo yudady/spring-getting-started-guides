893ba304ff6f74bfc4bb0ae2ae0a9fd71ca233ed
tommy <yudady@gmail.com>
2022-10-08 11:50:28 +0800

    spring-02

--- springboot2/springboot01/spring-02/src/main/java/io/github/yudady/bean/User.java
+++ springboot2/springboot01/spring-02/src/main/java/io/github/yudady/bean/User.java
@@ -4,12 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 @Data
 @ToString
@@ -17,13 +12,12 @@ import org.springframework.stereotype.Component;
 //@Configuration(proxyBeanMethods = true)
 //@ConfigurationProperties(prefix = "my")
 @ConfigurationProperties("my")
-
 public class User {
 
-//	@Value("${my.name}")
+	//	@Value("${my.name}")
 	public String name;
 
-//	@Value("${my.age}")
+	//	@Value("${my.age}")
 	public Integer age;
 
 }
