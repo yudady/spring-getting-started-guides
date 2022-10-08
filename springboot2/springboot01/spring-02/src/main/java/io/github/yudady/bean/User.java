de1ba9ffb83e33e2a5e8d26ebda93be123d6a117
tommy <yudady@gmail.com>
2022-10-08 13:14:59 +0800

    update spring-02

--- springboot2/springboot01/spring-02/src/main/java/io/github/yudady/bean/User.java
+++ springboot2/springboot01/spring-02/src/main/java/io/github/yudady/bean/User.java
@@ -9,15 +9,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ToString
 @EqualsAndHashCode
-//@Configuration(proxyBeanMethods = true)
-//@ConfigurationProperties(prefix = "my")
 @ConfigurationProperties("my")
 public class User {
 
-	//	@Value("${my.name}")
 	public String name;
 
-	//	@Value("${my.age}")
 	public Integer age;
 
 }
