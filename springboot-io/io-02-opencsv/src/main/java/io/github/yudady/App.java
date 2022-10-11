e519d9150491369535d278a248bc8060a1be5621
tommy <tommy.lin@yxunistar.com>
2022-10-11 17:13:13 +0800

    io-02-opencsv

--- springboot-io/io-02-opencsv/src/main/java/io/github/yudady/App.java
+++ springboot-io/io-02-opencsv/src/main/java/io/github/yudady/App.java
@@ -1,29 +1,46 @@
 package io.github.yudady;
 
 
-import io.github.yudady.bean.User;
-import lombok.extern.slf4j.Slf4j;
-
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
-@Slf4j
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
 	@Autowired
-	ApplicationContext context;
-
-	@Override
-	public void run(String... args) throws Exception {
-		User user = context.getBean(User.class);
-		log.info("[LOG] user : {}", user);
-	}
+	ResourceLoader resourceLoader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		// read
+		Resource userCsv = resourceLoader.getResource("classpath:user.csv");
+		CSVReader reader = new CSVReader(new FileReader(userCsv.getFile()));
+		List<String[]> rows = reader.readAll();
+		for (String[] row : rows) {
+			System.out.println(row[0] + "," + row[1] + "," + row[2]);
+		}
+
+		// write
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(new File("C:/Users/tommy.lin/Desktop/data.csv")));
+		csvWriter.writeNext(new String[]{"John", "Doe", "jdoe@example.com", "Sales"});
+		csvWriter.writeNext(new String[]{"Jane", "Doe", "jane.doe@example.com", "HR"});
+		csvWriter.close();
+	}
+
 }
