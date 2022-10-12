6a44ab8d7dd6b9e78f6e3f2de6507ed05f145023
tommy <tommy.lin@yxunistar.com>
2022-10-12 12:12:44 +0800

    add io-05-export-pdf

---
+++ springboot-io/io-05-export-pdf/src/main/java/com/knf/dev/model/Employee.java
@@ -0,0 +1,54 @@
+package com.knf.dev.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	protected Employee() {
+	}
+
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+}
