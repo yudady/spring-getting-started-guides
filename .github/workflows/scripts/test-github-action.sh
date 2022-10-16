7dab5b1b3f867156618bf1c341e199d8c662db1f
tommy <yudady@gmail.com>
2022-10-16 10:56:03 +0800

    test test-github-action 5

--- .github/workflows/scripts/test-github-action.sh
+++ .github/workflows/scripts/test-github-action.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env sh
-
-
 echo "test-github-action.sh =================================="
 echo "test-github-action.sh =================================="
 echo "test-github-action.sh =================================="
