2a4ce6b727f6e2f1eec23491edcfb2c0afefb3df
tommy <yudady@gmail.com>
2022-10-17 10:04:49 +0800

    add web-07-prometheus

---
+++ springboot-web/web-07-prometheus/src/test/java/yudady/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package yudady.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
