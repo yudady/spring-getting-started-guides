69ad857d6ae4cd7562e1e4e57a116220832847f4
tommy <yudady@gmail.com>
2022-10-17 12:02:34 +0800

    update web-07-prometheus

--- springboot-web/web-07-prometheus/src/main/java/yudady/demo/DemoApplication.java
+++ springboot-web/web-07-prometheus/src/main/java/yudady/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package yudady.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	/**
+	 * https://www.cnblogs.com/cjsblog/p/11556029.html
+	 */
+	@Bean
+	public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", DemoApplication.class.getName());
 	}
 
 }
