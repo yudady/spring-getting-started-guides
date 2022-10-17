2eec2ddb59beadd8276eb9af0f31fe0f6c8564cf
tommy <yudady@gmail.com>
2022-10-17 12:23:42 +0800

    move data

---
+++ springboot-devops/devops-02-gradle/src/main/java/devops02/App.java
@@ -0,0 +1,33 @@
+package devops02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class App {
+
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			log.info("CommandLineRunner................." + restTemplate.hashCode());
+		};
+	}
+}
