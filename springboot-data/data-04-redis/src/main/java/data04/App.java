ced351c9439ddaa3a2ff665ee3457f6152de9465
tommy <yudady@gmail.com>
2022-10-21 23:55:43 +0800

    init data-04-redis

--- springboot-data/data-04-redis/src/main/java/data04/App.java
+++ springboot-data/data-04-redis/src/main/java/data04/App.java
@@ -1,37 +1,53 @@
 package data04;
 
+import io.github.yudady.spring.StringBeans;
 import io.github.yudady.util.Exceptions;
+import io.github.yudady.util.Strings;
 import java.time.Duration;
 
 import io.github.yudady.util.Threads;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
-public class App implements ApplicationRunner {
+public class App implements ApplicationRunner, CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-
+    @Autowired
+    ApplicationContext applicationContext;
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
 
     public static void main(String[] args) {
-
         SpringApplication.run(App.class, args);
-
     }
 
     @Override
     public void run(ApplicationArguments args) {
+        StringBeans.getBeanDefinitions(applicationContext)
+            .forEach(entry -> System.out.println(Strings.format("{} : {}", entry.getKey(), entry.getValue().getClass().getName())));
+    }
+
+    @Override
+    public void run(String... args) {
+        //2、查看容器里面的组件
+
         for (int i = 0; i < 10; i++) {
             try {
                 Threads.sleepRoughly(Duration.ofSeconds(1));
@@ -43,6 +59,5 @@ public class App implements ApplicationRunner {
             }
 
         }
-
     }
 }
