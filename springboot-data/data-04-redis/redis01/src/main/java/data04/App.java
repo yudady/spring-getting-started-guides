eddc642f78b14062e92a284c753db6ce40f62cc3
tommy <yudady@gmail.com>
2022-10-22 08:51:24 +0800

    update data-04-redis

--- springboot-data/data-04-redis/redis01/src/main/java/data04/App.java
+++ springboot-data/data-04-redis/redis01/src/main/java/data04/App.java
@@ -3,17 +3,11 @@ package data04;
 import io.github.yudady.spring.StringBeans;
 import io.github.yudady.util.Exceptions;
 import io.github.yudady.util.Strings;
-import java.time.Duration;
-
 import io.github.yudady.util.Threads;
-import java.util.Arrays;
-import java.util.List;
+import java.time.Duration;
 import java.util.Locale;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,7 +15,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
