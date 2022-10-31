0baf06b9b98e93ba0b5cd610ddc9c2e4163a0bc3
tommy <yudady@gmail.com>
2022-10-31 10:15:43 +0800

    spring properties closed #7

--- springboot-k8s/k8s-01-frontend-backend/frontend/src/main/java/k8s01/frontend/service/ConsumerRestTemplateService.java
+++ springboot-k8s/k8s-01-frontend-backend/frontend/src/main/java/k8s01/frontend/service/ConsumerRestTemplateService.java
@@ -3,6 +3,7 @@ package k8s01.frontend.service;
 import k8s01.frontend.model.ConsumerKey;
 import k8s01.frontend.model.ConsumerUser;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +13,36 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ConsumerRestTemplateService {
 
+    @Value("${backend.url}")
+    String backend;
 
-	public Map<Integer, ConsumerUser> getUser() {
+    public Map<Integer, ConsumerUser> getUser() {
 
-		RestTemplate restTemplate = new RestTemplate(); // ! check  inject
-		ResponseEntity<Map<Integer, ConsumerUser>> response = restTemplate.exchange("http://localhost:9090/getUser",
-			HttpMethod.GET, null, new ParameterizedTypeReference<Map<Integer, ConsumerUser>>() {
-			});
-		Map<Integer, ConsumerUser> users = response.getBody();
+        RestTemplate restTemplate = new RestTemplate(); // ! check  inject
+        String url = backend + "/getUser";
+        System.out.println("url = " + url);
+        System.out.println("url = " + url);
+        System.out.println("url = " + url);
+        System.out.println("url = " + url);
+        System.out.println("url = " + url);
+        ResponseEntity<Map<Integer, ConsumerUser>> response = restTemplate.exchange(url,
+            HttpMethod.GET, null, new ParameterizedTypeReference<Map<Integer, ConsumerUser>>() {
+            });
+        Map<Integer, ConsumerUser> users = response.getBody();
 
-		return users;
+        return users;
 
-	}
+    }
 
-	public String deleteuser(Integer UserId) {
+    public String deleteuser(Integer UserId) {
 
-		RestTemplate restTemplate = new RestTemplate();
-		ConsumerKey key = new ConsumerKey();
-		key.setKey(UserId);
-		String url = "http://localhost:9090/deleteUser";
-		String commonResult = restTemplate.postForObject(url, key, String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        ConsumerKey key = new ConsumerKey();
+        key.setKey(UserId);
+        String url = backend + "/deleteUser";
+        String commonResult = restTemplate.postForObject(url, key, String.class);
 
-		return commonResult;
-	}
+        return commonResult;
+    }
 
 }
