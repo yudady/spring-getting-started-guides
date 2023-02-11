a2a54bd4f20abc6899332319f8371fe9f5f23045
tommy <yudady@gmail.com>
2023-02-11 09:58:42 +0800

    rabbitmq

--- springboot-message/rabbitmq/rabbitmq/springboot-rabbit-producer/src/test/java/cn/bufanli/ProducerTest.java
+++ springboot-message/rabbitmq/rabbitmq/springboot-rabbit-producer/src/test/java/cn/bufanli/ProducerTest.java
@@ -1,12 +1,10 @@
 package cn.bufanli;
 
 import cn.bufanli.config.RabbitMQConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author yd
@@ -14,12 +12,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date 2020/12/5 21:34
  */
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
 public class ProducerTest {
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
     @Test
-    public void testSend(){
-        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"boot.hello","boot mq hello ");
+    public void testSend() {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "boot.hello", "boot mq hello ");
     }
 }
