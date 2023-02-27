070acd2c26548de934b3e65f9df75261090af754
tommy <yudady@gmail.com>
2023-02-27 19:57:19 +0800

    update

--- springboot-io/io-02-csv/export-apache-csv/src/main/java/io02/export/apachecsv/controller/EmployeeController.java
+++ springboot-io/io-02-csv/export-apache-csv/src/main/java/io02/export/apachecsv/controller/EmployeeController.java
@@ -1,17 +1,18 @@
 package io02.export.apachecsv.controller;
 
 import io02.export.apachecsv.service.EmployeeService;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+
+@RestController
 public class EmployeeController {
 
     @Autowired
@@ -21,10 +22,11 @@ public class EmployeeController {
     public ResponseEntity<InputStreamResource> exportCSV(HttpServletResponse response) throws Exception {
 
         String filename = "employees.csv";
-        InputStreamResource file = new InputStreamResource(employeeService.load());
+        ByteArrayInputStream load = employeeService.load();
+        InputStreamResource file = new InputStreamResource(load);
 
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-            .contentType(MediaType.parseMediaType("application/csv")).body(file);
+                .contentType(MediaType.parseMediaType("application/csv")).body(file);
 
     }
 }
