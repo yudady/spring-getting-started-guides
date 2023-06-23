c3724cec65de4c66de968cdaf8a21de5649570c2
tommy <yudady@gmail.com>
2023-06-23 18:30:29 +0800

    20230623

--- springboot-data/data-01-start/src/main/java/io/github/yudady/Data01StartApp.java
+++ springboot-data/data-01-start/src/main/java/io/github/yudady/Data01StartApp.java
@@ -3,7 +3,6 @@ package io.github.yudady;
 
 import io.github.yudady.bean.User;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,18 +11,18 @@ import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication
-public class App implements CommandLineRunner {
+public class Data01StartApp implements CommandLineRunner {
 
-	@Autowired
-	ApplicationContext context;
+    @Autowired
+    ApplicationContext context;
 
-	@Override
-	public void run(String... args) throws Exception {
-		User user = context.getBean(User.class);
-		log.info("[LOG] user : {}", user);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        User user = context.getBean(User.class);
+        log.info("[LOG] user : {}", user);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Data01StartApp.class, args);
+    }
 }
